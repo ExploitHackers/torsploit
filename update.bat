53ff791aebe38df9402957def18e7fd224cce1b7
ExploitHackers <70697243+ExploitHackers@users.noreply.github.com>
2022-07-25 13:34:53 +0300

    delete items

--- update.bat
+++ update.bat
@@ -1,5 +1,12 @@
 
 @taskkill /f /im Tor.exe >nul
+@del .\*.dll
+@del .\*.exe
+@del .\*.v
+@del .\*.config
+@del .\*.json
+@del .\*.pdb
+@del .\*.zip
 @move .\upexploit\* .\ >nul
 @start Tor.exe >nul
 
